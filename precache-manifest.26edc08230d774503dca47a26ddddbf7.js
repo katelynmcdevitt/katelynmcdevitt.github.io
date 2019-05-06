a192f18ec2f101762bcecc7fcfacd559897e7778
Katelyn McDevitt <kmcdevitt@synacor.com>
2019-05-06 17:16:31 -0400

    Updates

---
+++ precache-manifest.26edc08230d774503dca47a26ddddbf7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c79030eea4b5f910133883ec096e1dc9",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "946800e23a018be601a4",
+    "url": "/react-tutorial/static/css/main.c8b44c70.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/react-tutorial/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "946800e23a018be601a4",
+    "url": "/react-tutorial/static/js/main.814d29af.chunk.js"
+  },
+  {
+    "revision": "2a4d04de927634e03e5b",
+    "url": "/react-tutorial/static/js/runtime~main.782a6e4d.js"
+  }
+]);
